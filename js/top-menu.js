88c00126d98df4a5c81077ce3c1884df74ec9f91
leeeydia <leeeydia@gmail.com>
2024-08-06 15:34:04 +0900

    2차 통합

---
+++ js/top-menu.js
@@ -0,0 +1,65 @@
+const API_KEY = '838d721e4cc805544c744295d5d45809';
+const BASE_URL = 'https://api.themoviedb.org/3/movie/';
+
+const options = {
+  method: 'GET',
+  headers: {
+    accept: 'application/json',
+    Authorization:
+      'Bearer eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiI4MzhkNzIxZTRjYzgwNTU0NGM3NDQyOTVkNWQ0NTgwOSIsIm5iZiI6MTcyMjQ5MzgxMi43MDA2ODksInN1YiI6IjY2YTJlYzljMDk5MDU0NTUxNDYxMmJlOSIsInNjb3BlcyI6WyJhcGlfcmVhZCJdLCJ2ZXJzaW9uIjoxfQ.CrYho_flSejWu4M0bLQq4uFADdDAPFUi-pX3fKlYLsc'
+  }
+};
+
+fetch(`${BASE_URL}top_rated?language=en-US&page=1`, options)
+  .then((response) => response.json())
+  .then((response) => {
+    renderTopMovies(response.results);
+  })
+  .catch((err) => console.error(err));
+
+// 영화 카드
+const createTopMovieCard = (movie) => {
+  const { poster_path } = movie;
+
+  const card = document.createElement('div');
+  const image = document.createElement('img');
+
+  card.className = 'movie-card';
+  image.src = `https://image.tmdb.org/t/p/w200${poster_path}`;
+
+  card.appendChild(image);
+  return card;
+};
+
+// 영화 목록
+const renderTopMovies = (movies) => {
+  const topMovieContainer = document.getElementById('TopMovieContainer');
+  topMovieContainer.innerHTML = '';
+
+  movies.forEach((movie) => {
+    const movieCard = createTopMovieCard(movie);
+    topMovieContainer.appendChild(movieCard);
+  });
+};
+
+// 영화 검색
+function search_movie(event) {
+  event.preventDefault();
+  const query = document.querySelector('.search-box').value.toLowerCase();
+
+  if (query) {
+    fetch(`${BASE_URL}top_rated?language=en-US&page=1`, options)
+      .then((response) => response.json())
+      .then((data) => {
+        const movies = data.results.filter((movie) => movie.title.toLowerCase().includes(query));
+        const movieContainer = document.getElementById('TopMovieContainer');
+        movieContainer.innerHTML = '';
+
+        movies.forEach((movie) => {
+          const card = createTopMovieCard(movie);
+          movieContainer.appendChild(card);
+        });
+      })
+      .catch((error) => console.error('Error:', error));
+  }
+}
